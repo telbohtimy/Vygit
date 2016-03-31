5cf491cdbe43bb6929c6729810832b68a7d3c674
telbohtimy <elbohtim@ualberta.ca>
2016-03-31 12:05:19 -0600

    a

--- vygit/settings.py
+++ vygit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '58xlgwe2*ib+b@jrhg$wpj75#r023v__m5-o#!7s7-ocz6fh#&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
