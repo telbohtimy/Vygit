8299f691a2b8c3511a1b27793e336201a66b760e
telbohtimy <elbohtim@ualberta.ca>
2016-03-31 10:56:42 -0600

    static files

--- vygit/settings.py
+++ vygit/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_PATH = os.path.join(BASE_DIR, 'wsgi','static')#os.path.abspath(os.path.join(BASE_DIR,'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi','static')#os.path.abspath(os.path.join(BASE_DIR,'static'))
 
 STATICFILES_DIRS = (
     #STATIC_PATH,
